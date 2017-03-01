7e001b71ed72e2ed7b0d704a5af8ddd8f004160d
Marina Nieblas <marina.nieblas@hotmail.com>
2017-03-01 21:27:11 +0100

    Método de alta de promos - falta recoger datos del formulario

--- src/main/java/com/markdirect/markdirect/controllers/PromoController.java
+++ src/main/java/com/markdirect/markdirect/controllers/PromoController.java
@@ -3,19 +3,19 @@ package com.markdirect.markdirect.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PromoController {
-
-	@RequestMapping(value="promos", method=RequestMethod.POST)
-	public String promos(@RequestParam("promo-titulo") String titulo,
-						@RequestParam("promo-descripcion") String descripcion,
-						@RequestParam("promo-validez") String validez,
-						@RequestParam("promo-img") String img,
-						@RequestParam("promo-zona") String zona,
-						@RequestParam("promo-edad") String edad,
-						@RequestParam("promo-sexo") String sexo) {
+	
+	@RequestMapping(value="promos", method=RequestMethod.GET)
+	public String promos() {
+		return "promos";
+	}
+	
+	
+	@RequestMapping(value="altaPromos", method=RequestMethod.GET)
+	public String altaPromos() {
+		System.out.println("Hola, doy de alta una promo");
 		return "promos";
 	}
 }
