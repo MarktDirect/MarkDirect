c2cfb9c50a3c9aa30671ac13b52482e6f4854ea9
CristianSE <cristiancarratraca@gmail.com>
2017-03-02 10:42:00 +0100

    Beans administrador completa

--- src/main/java/com/markdirect/markdirect/beans/Administrador.java
+++ src/main/java/com/markdirect/markdirect/beans/Administrador.java
@@ -1,5 +1,47 @@
 package com.markdirect.markdirect.beans;
 
 public class Administrador {
+	//Variables
+	private String usuario;
+	private String password;
+	
+	
+	//Constructor
+	public Administrador(){};
+	public Administrador(String usuario, String password) {
+		this.usuario = usuario;
+		this.password = password;
+	}
+	
+	//Metodos
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Administrador [usuario=");
+		builder.append(usuario);
+		builder.append(", password=");
+		builder.append(password);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
 
 }
