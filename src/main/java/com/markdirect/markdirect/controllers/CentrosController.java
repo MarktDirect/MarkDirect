102de10be8aaf6faa54cf3c0d8e54b6816f5c1ce
carlotapozo <carlota.pozobalbin@gmail.com>
2017-03-02 12:21:16 +0100

    subo controlador

--- src/main/java/com/markdirect/markdirect/controllers/CentrosController.java
+++ src/main/java/com/markdirect/markdirect/controllers/CentrosController.java
@@ -4,16 +4,28 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class CentrosController {
 	
-	@Autowired
-	private HttpSession session;
 	
-	
-	@RequestMapping("/altaCentro")
-	public String altaCentro(){
-		return "altaCentro";
+	@RequestMapping(value="centros", method=RequestMethod.GET)
+	public String centros(){
+		return "centros";
 	}
+	
+	@RequestMapping(value="altaCentro", method=RequestMethod.POST)
+	public String altaPromos(@RequestParam("centro-nombre") String nombre,
+						@RequestParam("centro-direccion") String direccion,
+						@RequestParam("centro-descripcion") String descripcion,
+						@RequestParam("centro-tipo") String tipo,
+						@RequestParam("centro-subtipo") String subtipo) {
+		
+		System.out.println("Hola, yo soy la el centro " + nombre + "y mi descripción es: " + descripcion);
+		
+		return "centros";
+	}
+	
 
 }
