85357240dc9d0fe0c43a55b5779796e203e27fc0
carlotapozo <carlota.pozobalbin@gmail.com>
2017-03-02 14:29:10 +0100

    He creado las sentencias sql

--- src/main/java/com/markdirect/markdirect/controllers/CentrosController.java
+++ src/main/java/com/markdirect/markdirect/controllers/CentrosController.java
@@ -7,27 +7,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CentrosController {
-	
-	
+
+
 	@RequestMapping(value="centros", method=RequestMethod.GET)
 	public String centros(){
+
+		//Hacemos consulta a base de datos para que nos cargue todos los centros que hay
+		String sql="SELECT * FROM centers";
+
+		
+
 		return "centros";
 	}
-	
+
 	@RequestMapping(value="altaCentro", method=RequestMethod.POST)
-	public String altaPromos(@RequestParam("centro-nombre") String nombre,
-						@RequestParam("centro-direccion") String direccion,
-						@RequestParam("centro-descripcion") String descripcion,
-						@RequestParam("centro-tipo") String tipo,
-						@RequestParam("centro-subtipo") String subtipo) {
-		
+	public String altaPromos(@RequestParam("centerNombre") String nombre,
+			@RequestParam("centerDireccion") String direccion,
+			@RequestParam("centerDescription") String descripcion,
+			@RequestParam("centerTipo") String tipo,
+			@RequestParam("centerSubtipo") String subtipo) {
+
 		System.out.println("Hola, yo soy la el centro " + nombre + "y mi descripción es: " + descripcion);
+
 		
+		String sql="INSERT INTO centers WHERE ('centerNombre','centerDireccion','centerDescription'"
+				+ " 'centerTipo', 'centerSubtipo') VALUES ('nombre','direccion','descripcion','tipo','subtipo'";
+
 		return "centros";
 	}
-	
+
 
 }
