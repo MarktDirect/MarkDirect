c5eb482765af09cb9be9a81cd826645402d8b617
CristianSE <cristiancarratraca@gmail.com>
2017-03-02 10:21:23 +0100

    Comentada tabla usuarios con BD

--- src/main/java/com/markdirect/markdirect/controllers/UsuariosController.java
+++ src/main/java/com/markdirect/markdirect/controllers/UsuariosController.java
@@ -1,9 +1,13 @@
 package com.markdirect.markdirect.controllers;
 
+import java.util.List;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
 
 //Controlador de la pagina de usuarios
 @Controller
@@ -14,6 +18,20 @@ public class UsuariosController {
 	@RequestMapping(value = "/usuarios", method = RequestMethod.GET)
 	public ModelAndView usuarios() {
 		ModelAndView mav = new ModelAndView();
+		mav.setViewName("usuarios");
+		
+		//Metodo para sacar la tabla en usuarios comentado
+		/*
+		 * JdbcTemplate db= new JdbcTemplate(Conector.getDataSource());
+		 * String SQL="SELECT*FROM Usuarios";
+		List<Usuario> listausuario = db.query(
+				SQL, new BeanPropertyRowMapper<Usuario>(Usuario.class));
+				mav.addObject("lista",listausuario);
+		 */
+		
+
+		
+		
 
 		return mav;
 	}
