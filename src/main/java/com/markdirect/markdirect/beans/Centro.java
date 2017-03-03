11df2ac70c63c87a41252e995da776ea9e61d8ed
carlotapozo <carlota.pozobalbin@gmail.com>
2017-03-03 09:27:53 +0100

    Creo la claseCentro

--- src/main/java/com/markdirect/markdirect/beans/Centro.java
+++ src/main/java/com/markdirect/markdirect/beans/Centro.java
@@ -2,5 +2,46 @@ package com.markdirect.markdirect.beans;
 
 public class Centro {
 
-	//La he liado y lo tengo que arreglar
+	private String subtipo;
+	private String tipo ;
+	private String descripción;
+	private String dirección;
+	private String Nombre;
+	public String getSubtipo() {
+		return subtipo;
+	}
+	public void setSubtipo(String subtipo) {
+		this.subtipo = subtipo;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getDescripción() {
+		return descripción;
+	}
+	public void setDescripción(String descripción) {
+		this.descripción = descripción;
+	}
+	public String getDirección() {
+		return dirección;
+	}
+	public void setDirección(String dirección) {
+		this.dirección = dirección;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Centro [subtipo=" + subtipo + ", tipo=" + tipo + ", descripción=" + descripción + ", dirección="
+				+ dirección + ", Nombre=" + Nombre + "]";
+	}
+	
+		
 }
