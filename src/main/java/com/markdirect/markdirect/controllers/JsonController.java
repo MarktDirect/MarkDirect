4ab0a81d683354dcd8b41f7ed3a26c4f3a0b340f
Marina Nieblas <marina.nieblas@hotmail.com>
2017-03-06 08:38:09 +0100

    Actualizado jabado método getFilteredGenericPromos

--- src/main/java/com/markdirect/markdirect/controllers/JsonController.java
+++ src/main/java/com/markdirect/markdirect/controllers/JsonController.java
@@ -42,6 +42,8 @@ public class JsonController {
 	/**
 	 * Método que realiza una consulta a la BBDD, lista todas las promociones genéricas filtradas
 	 * por género y edad y devuelve la lista en formato JSON. Recibe los parámetros desde la url
+	 * @param promoGen - género del usuario
+	 * @param userAge - edad del usuario
 	 * @return List<Promocion> - promociones que le corresponden en función al filtro realizado
 	 */
 	@RequestMapping(value="getFilteredGenericPromos/{promoGen}/{userAge}", method=RequestMethod.GET)
@@ -64,7 +66,6 @@ public class JsonController {
 	@RequestMapping(value="getLocationPromos/{controlzoneMajor}/{controlzoneMinor}/{userId}/{promoGen}/{userAge}", method=RequestMethod.GET)
 	public @ResponseBody List<Promocion> getLocationPromos(@PathVariable("controlzoneMajor") int controlzoneMajor, @PathVariable("controlzoneMinor") int controlzoneMinor, @PathVariable("userId") String userId, @PathVariable("promoGen") String promoGen, @PathVariable("userAge") int userAge) {
 		//Obtenemos la lista entera de promociones del método que tenemos en DatabaseMarkDirect
-		System.out.println("entro por el requestmapping");
 		List<Promocion> promociones = db.listLocationPromos(promoGen, userAge, controlzoneMajor, userId, controlzoneMinor);
 		return promociones;
 	}
